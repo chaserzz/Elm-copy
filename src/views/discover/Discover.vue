db966e9af623423728c3976bf9d093a8fdf17a6e
Chaser <liuzhian0515@163.com>
2020-07-17 20:22:35 +0800

    搜索页面上半部分完成

--- src/views/discover/Discover.vue
+++ src/views/discover/Discover.vue
@@ -1,7 +1,11 @@
 <!-- 组件说明 -->
 <template>
   <div class=''>
-    Discover
+    <discover-nav-bar />
+    <div class='search'>
+        <input type="text" placeholder="请输入商家或美食名称" v-model='keyword' />
+        <button>提交</button>
+    </div>
     <scroll id='Scroll'>
 
     </scroll>
@@ -9,12 +13,16 @@
 </template>
 
 <script>
+    import DiscoverNavBar from './childCom/DiscoverNavBar.vue'
+
     import Scroll from 'components/common/scroll/scroll.vue'
 
     export default {
         name: 'Discover',
         components: {
-            Scroll
+            Scroll,
+            DiscoverNavBar,
+            keyword: ''
         },
         data() {
             return {
@@ -33,5 +41,35 @@
 <style scoped>
     #Scroll {
         height: 50vh;
+    }
+    
+    .search {
+        padding: 0.55rem 0.15rem;
+        height: 3rem;
+    }
+    
+    .search input {
+        width: 73%;
+        height: 100%;
+        margin-left: 2%;
+        border: 0.035rem solid #e4e4e4;
+        border-radius: 0.15rem;
+        padding: 0.45rem;
+        background-color: rgb(242, 242, 242);
+        font-size: 0.85rem;
+        font-weight: 560;
+        color: rgb(242, 242, 242);
+    }
+    
+    .search button {
+        display: inline-block;
+        width: 22%;
+        margin-left: 2%;
+        height: 100%;
+        background-color: rgb(49, 144, 232);
+        color: #fff;
+        font-size: 0.8rem;
+        font-weight: 555;
+        border-radius: 0.2rem;
     }
 </style>
---
+++ src/views/discover/childCom/DiscoverNavBar.vue
@@ -0,0 +1,56 @@
+<!-- 组件说明 -->
+<template>
+  <div>
+    <nav-bar class='navBar'>
+      <div class='left' slot="left">
+        <svg t="1594982764660" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2066" width="22" height="22"><path d="M256 512l512 512 0-135.776-376.224-376.224 376.224-376.256 0-135.745z" p-id="2067" fill="#ffffff"></path></svg>
+      </div>
+      <div class='center' slot="center">
+        <span>搜索</span>
+      </div>
+      <div class='right' slot="right"></div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+    import NavBar from 'components/common/navbar/NavBar'
+    export default {
+        name: 'DiscoverNavBar',
+        components: {
+            NavBar
+        },
+        data() {
+            return {
+
+            };
+        },
+        computed: {
+
+        },
+        methods: {
+
+        },
+    }
+</script>
+
+<style scoped>
+    .navBar {
+        background-color: rgb(49, 144, 232);
+    }
+    
+    .left {
+        display: flex;
+        margin-left: .1rem;
+    }
+    
+    .center {
+        font-size: 1.1rem;
+        font-weight: 600;
+        color: #fff;
+    }
+    
+    .right {
+        width: 2rem;
+    }
+</style>
